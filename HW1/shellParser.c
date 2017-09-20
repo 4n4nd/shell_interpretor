c89856093435d4b5dc9954e8280c17aaef4da8d6
Anand Sanmukhani <asan@bu.edu>
2017-09-20 14:34:30 -0400

    removed strsep() and used simple if statements to separate commands

--- HW1/shellParser.c
+++ HW1/shellParser.c
@@ -6,17 +6,49 @@ int EXIT = 0;	//Main Global Variable to control the state of the state machine
 
 int main(){
 	int myInt = 0;
-	char *command;
-	// string command;
-	while(!EXIT){
-		printf("\e[91m""bash@anand~# ""\e[0m");
-		fgets(command,100,stdin);	
 
-		if(!strncmp("exit",command,4))
+	// string command;
+	
+	while(!EXIT){
+		// char command[100];
+		char command[1000];// = &comArray;
+		char **commandList;
+
+		printf("\e[36m""bash@anand~# ""\e[0m"); 
+		fgets(command,100,stdin);
+		int stringLength = strlen(command)-1;
+
+		const char delim1 = ' ';
+		const char delim2 = '	';
+		
+		// printf("Command -> %s\n",strsep(&command,&delim1));
+		int commandState = 0;
+		int piper=0;
+		for (int i = 0; i < strlen(command)-1; ++i)
 		{
-			EXIT=1;
-			printf("Bye!\n");
+			if (command[i]!=' ' && command[i]!='&' && command[i]!='|' && command[i]!='	')
+			{
+				// printf("Break Here.\n");
+				printf("%c\n", command[i]);
+
+			}else
+			{
+				printf("Break Here.\n");
+
+			}
 		}
+
+		// printf("The arraySize is: %d\n",stringLength);
+		// if (command!=NULL)
+		// {
+		// 	printf("Attributes -> %s\n",command);
+		// 	if(!strncmp("exit",command,4))
+		// 	{
+		// 		EXIT=1;
+		// 		printf("Bye!\n");
+		// 	}
+		// }
+		
 	}
 	
 	return 0;

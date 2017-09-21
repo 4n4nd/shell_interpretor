d8c41165a0eb5e06f94ec194f220b1bb960dbed7
Anand Sanmukhani <asan@bu.edu>
2017-09-21 01:49:27 -0400

    added comments and an exit command to quit the shell

--- HW1/shellParser.c
+++ HW1/shellParser.c
@@ -11,23 +11,23 @@ int main(){
 	
 	while(!EXIT){
 		// char command[100];
-		char command[1000];// = &comArray;
-		char commandList[100][500]={{'\0'}};
+		char command[1000];// = &comArray; //Buffer Array to store user input stream
+		char commandList[100][500]={{'\0'}}; // Command List to tokenize commands including white spaces
 
-		printf("\e[36m""bash@anand~# ""\e[0m"); 
-		fgets(command,1000,stdin);
-		int stringLength = strlen(command)-1;
+		printf("\e[36m""bash@anand~# ""\e[0m"); //User Bash input custom
+		// printf("myParser>");		//User Bash input traditional
+		fgets(command,1000,stdin); //Take user input and sore it to command buffer
 
 		// const char delim1 = ' ';
 		// const char delim2 = '	';
 		
 		// printf("Command -> %s\n",strsep(&command,&delim1));
-		int commandState = 0;
-		int piper=0;
-		int stringIter = 0;
+		int commandState = 0;		//iterator to change to next token
+		// int piper=0;
+		int stringIter = 0;	// iterator for next character
 		for (int i = 0; i < strlen(command)-1; ++i)
 		{
-			if (command[i]!=' ' && command[i]!='&' && command[i]!='|' && command[i]!='	'&& command[i]!='>' && command[i]!='<')
+			if (command[i]!=' ' && command[i]!='&' && command[i]!='|' && command[i]!='	'&& command[i]!='>' && command[i]!='<')	// excluding meta characters
 			{
 				commandList[commandState][stringIter]=command[i];
 				stringIter++;
@@ -39,7 +39,7 @@ int main(){
 				// printf("%i\n", i);
 				stringIter=0;
 				commandState++;
-				if (command[i]=='&'||command[i]=='|'||command[i]=='>'||command[i]=='<')
+				if (command[i]=='&'||command[i]=='|'||command[i]=='>'||command[i]=='<')	//excluding white spaces
 				{
 					// commandState++;
 					commandList[commandState][stringIter]=command[i];
@@ -53,7 +53,7 @@ int main(){
 		}
 		int tokenIter=0;
 		char tokenArray[100][100]={{'\0'}};
-		for (int i = 0; i < commandState+1; ++i)
+		for (int i = 0; i < commandState+1; ++i)			//loop to remove empty items
 		{
 			// printf("%i\n",strncmp(commandList[i],"",1));
 			if (strncmp(commandList[i],"",1)!=0)
@@ -65,7 +65,7 @@ int main(){
 		}
 		int state =0;
 		char printedString[20] = "Command";
-		for (int i = 0; i < tokenIter; ++i)
+		for (int i = 0; i < tokenIter; ++i)			// state machine to categorize all tokens for output
 		{	
 			if (state==1)
 			{
@@ -95,6 +95,10 @@ int main(){
 				
 			}
 			printf("%s - %s\n",tokenArray[i],printedString );
+			if (strcmp(tokenArray[i],"exit")==0)
+			{
+				EXIT=1;
+			}
 		}
 
 		// printf("The arraySize is: %d\n",stringLength);
